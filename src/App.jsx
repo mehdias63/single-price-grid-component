44dcf6b3141a58808ef21607f4fb94f35228cfb0
mehdias63 <137681653+mehdias63@users.noreply.github.com>
2024-01-15 13:46:36 +0330

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import Single from './components/Single'
-
+import React from "react";
+import Single from "./components/Single";
 
 function App() {
-
   return (
-    <main className='bg-[#E6EFF5] flex justify-center items-center p-20 font-[karla] md:flex-row'>
+    <main className="bg-[#E6EFF5] flex justify-center items-center p-20 font-[karla] md:flex-row md:min-h-screen">
       <Single />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
